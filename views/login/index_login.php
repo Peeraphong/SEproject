66e672320b6b66986b2f7e73e40d7e717885b8f5
NapatsornJR <napatsorn.ji@ku.th>
2022-03-25 01:51:06 +0700

    css login

--- views/login/index_login.php
+++ views/login/index_login.php
@@ -10,13 +10,14 @@
     <div class="window">
       <form class="container" name="frmlogin"  method="post" action="?controller=login&action=check" enctype="multipart/form-data">
         <p> </p>
-        <p><b> Login </b></p>
-        <p> ชื่อผู้ใช้ :
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/97/KU_Logo.png" width="100px"/>
+        <br><h3> เข้าสู่ระบบ KU </h3>
+        <p><b> ชื่อผู้ใช้ &nbsp;&nbsp;:
           <input type="text" required name="Username" placeholder="Username">
-        </p>
-        <p>รหัสผ่าน :
+        </b></p>
+        <p><b>รหัสผ่าน :
           <input type="password" required name="Password" placeholder="Password">
-        </p>
+        </b></p>
         <p>
           <button type="submit" name="action" value="Login">Login</button>
           
@@ -27,12 +28,52 @@
 </body>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=K2D:wght@300&display=swap');
+
+
   .container {
-    margin-left: 10em;
+    height: 450px;
+    width: 400px;
+    margin-left: 50%;
+    margin-right: auto;
     text-align: center;
     font-size: 20px;
+    background-image: linear-gradient(#c5e3ec, white);
+    border-style: solid;
+    border-width: 1px;
+    border-color: grey;
+    border-radius: 30px;
+    font-family: 'K2D', sans-serif;
   }
 
+  input {
+    font-family: 'K2D', sans-serif;
+    border-radius: 10px;
+    width: 50%;
+  }
+
+  h3 {
+    font-size: 28px;
+    color: #34a0a4;
+    text-shadow: #000 0px 0px 1px,   #000 0px 0px 1px,
+                 #000 0px 0px 1px,   #000 0px 0px 1px;
+  }
+
+  button {
+    font-family: 'K2D', sans-serif;
+    color: white;
+    font-weight: bold;
+    padding: 1em;
+		background-image: linear-gradient(to right, #168aad, #76c893);
+		border: none;
+		border-radius: 30px;
+		font-weight: 600;
+    margin-top: 20px;
+  }
+
+  button:hover {
+    background-color: #76c893;
+  }
   
 
 </style>
